2e7616beb9ea4b4a0c2c3505de3e7fb443673b29
Phil Whelan <phil123@gmail.com>
2013-01-25 16:40:02 -0800

    differentiate utm_content

--- app/controllers/clicked_controller.rb
+++ app/controllers/clicked_controller.rb
@@ -1,13 +1,10 @@
 class ClickedController < ApplicationController
   def a
-    fluidgoal "read-a"
-    done
+    fluidgoal "read-sfror-a"
+    redirect_to "https://www.fluidfeatures.com/?utm_source=sfror&utm_medium=mailinglist&utm_content=rails-a-b-testing&utm_campaign=get-word-out"
   end
   def b
-    fluidgoal "read-b"
-    done
-  end
-  def done
-    redirect_to "https://www.fluidfeatures.com"
+    fluidgoal "read-sfror-b"
+    redirect_to "https://www.fluidfeatures.com/?utm_source=sfror&utm_medium=mailinglist&utm_content=rails-a-b-testing&utm_campaign=get-word-out"
   end
 end
